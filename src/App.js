8cb8a681bbe9a799540a83f12af38db43988f8f9
Zac <zacaree@gmail.com>
2019-05-03 12:28:17 -0500

    Like button finished!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import BtnLike from './BtnLike';
 
+// data represents data fetched from a database
+const data = {
+  user: {
+    name: 'Zac Remboldt',
+    likedPosts: [13482, 17423, 21233],
+    social: {
+      website: 'https://www.zacremboldt.com/',
+      github: '@zacaree',
+      codepen: '@zacaree',
+      twitter: '@zremboldt'
+    }
+  }
+};
+
 function App() {
   return (
     <div className="App">
-      <BtnLike />
+      <BtnLike user={data.user} postId={12345} />
     </div>
   );
 }
--- src/BtnLike.js
+++ src/BtnLike.js
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
-import heart from './images/heart.svg';
+import Heart from './images/heart.js';
 
-const BtnLike = () => {
+const BtnLike = props => {
+  const { postId } = props;
+  const { likedPosts } = props.user;
+
   const [likeCount, setLikeCount] = useState(255);
+  const liked = likedPosts.includes(postId) ? true : false;
+
+  const handleClick = () => {
+    if (!liked) {
+      likedPosts.push(postId); // add this postId to the user's list of likes.
+      setLikeCount(likeCount + 1); // add 1 to the total like count.
+    } else {
+      const i = likedPosts.indexOf(postId); // find the index of this postId.
+      likedPosts.splice(i, 1).join(); // remove this postId from the user's list of likes.
+      setLikeCount(likeCount - 1); // remove 1 from the total like count.
+    }
+  };
 
   return (
-    <button onClick={() => setLikeCount(likeCount + 1)}>
-      <img src={heart} className="App-logo" alt="logo" />
+    <button onClick={handleClick} className={liked ? 'liked' : ''}>
+      <Heart />
       <span>{likeCount}</span>
     </button>
   );
